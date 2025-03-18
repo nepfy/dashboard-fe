81e07d7589675f33eafb0f7364c01b1fe645731b
Nepfy <nepfy@MacBook-Pro-de-Rafael.local>
2025-03-17 23:59:35 -0300

    fix: build

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,3 @@
-import Image from "next/image";
-
 export default function Home() {
   return (
     <div className="grid grid-rows-[auto_1fr_auto] items-center justify-items-center min-h-screen p-8 pb-20 gap-8 sm:p-20">
