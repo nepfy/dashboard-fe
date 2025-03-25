10893c2f7de18591f9bdedb6391a830d80590b70
Nepfy <nepfy@MacBook-Pro-de-Rafael.local>
2025-03-25 15:46:42 -0300

    chore: add styling to 404 page

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,21 +1,19 @@
 import Link from "next/link";
+import "../styles/404.css";
 
 export default function Custom404() {
   return (
-    <div className="grid place-items-center pt-20 pb-10 sm:pb-0 sm:pt-0 sm:min-h-screen">
-      <div className="flex items-center justify-center p-8 sm:p-20 pb-0 sm:pb-20">
-        <div className="w-full max-w-[480px] space-y-8">
-          <div className="space-y-2">
-            <h2 className="text-[32px] font-bold text-[var(--color-white-neutral-light-800)] text-center">
+    <div className="error-container">
+      <div className="error-content">
+        <div className="error-wrapper">
+          <div className="error-text-container">
+            <h2 className="error-title">
               Página que você procura não foi encontrada
             </h2>
-            <p className="text-[var(--color-white-neutral-light-500)] text-center">
+            <p className="error-description">
               Verifique se o endereço está correto ou entre em contato com o
               suporte.
-              <Link
-                href="/"
-                className="text-[var(--color-white-neutral-light-500)]"
-              >
+              <Link href="/" className="error-link">
                 Voltar
               </Link>
             </p>
