8da12451a1e6a97fa394c10fff025a29c613816a
Nepfy <rafael.dantas.br@gmail.com>
2025-05-18 12:29:11 -0300

    feat: add lottie animation

--- src/components/IntroSlider/index.tsx
+++ src/components/IntroSlider/index.tsx
@@ -1,4 +1,7 @@
-import Ilustration from "#/components/icons/Ilustration";
+import Lottie from "lottie-react";
+import criarProposta from "#/lotties/criar-proposta.json";
+// import criarCalculadora from "#/lotties/criar-calculadora.json";
+// import criarContrato from "#/lotties/criar-contrato.json";
 
 interface IntroSliderProps {
   title?: string;
@@ -7,21 +10,40 @@ interface IntroSliderProps {
 
 export default function IntroSlider({ title, description }: IntroSliderProps) {
   return (
-    <div className="hidden bg-[var(--color-primary-light-400)] xl:flex items-center justify-center">
-      <div className="w-full h-full flex items-center justify-center p-8">
-        <div className="w-full max-w-[80%]">
-          <div className="w-full aspect-[622/714] relative">
-            <Ilustration className="w-full h-full" />
-            <div className="absolute sm:bottom-0 lg:bottom-2xl xl:bottom-9xl left-0 right-0 space-y-2">
-              <h2 className="text-[32px] font-medium text-[var(--color-white-neutral-light-200)] text-center">
-                {title}
-              </h2>
-              <p className="text-[var(--color-primary-light-200)] text-center max-w-72 mx-auto">
-                {description}
-              </p>
-            </div>
-          </div>
-        </div>
+    <div className="hidden xl:flex flex-col items-center justify-center lootie-bg relative h-full w-full max-w-[1038px]">
+      <div className="w-full h-full absolute inset-0">
+        <Lottie
+          animationData={criarProposta}
+          className="w-full h-full"
+          rendererSettings={{
+            preserveAspectRatio: "xMidYMid slice",
+          }}
+        />
+
+        {/* <Lottie
+          animationData={criarCalculadora}
+          className="w-full h-2/3"
+          rendererSettings={{
+            preserveAspectRatio: "xMidYMid slice",
+          }}
+        /> */}
+
+        {/* <Lottie
+          animationData={criarContrato}
+          className="w-full h-2/3"
+          rendererSettings={{
+            preserveAspectRatio: "xMidYMid slice",
+          }}
+        /> */}
+      </div>
+
+      <div className="w-[234px] 2xl:w-[260px] text-center absolute z-10 lg:bottom-[25%] 2xl:bottom-[30%]">
+        <h3 className="text-[24px] 2xl:text-[28px] text-white-neutral-light-200 font-medium">
+          {title}
+        </h3>
+        <p className="text-primary-light-200 text-sm 2xl:text-[16px]">
+          {description}
+        </p>
       </div>
     </div>
   );
