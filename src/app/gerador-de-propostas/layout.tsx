25309a9e434c46c311255c7b5239cf1a751e1642
Nepfy <rafael.dantas.br@gmail.com>
2025-05-25 17:58:55 -0300

    feat: add layout to the project generator page

---
+++ src/app/gerador-de-propostas/layout.tsx
@@ -0,0 +1,44 @@
+import Link from "next/link";
+
+import CloseIcon from "#/components/icons/CloseIcon";
+import Logo from "#/components/icons/Logo";
+
+export default function ProjectGenerator({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <div className="bg-white-neutral-light-200">
+      <nav className="p-7 border-b border-b-white-neutral-light-300 flex items-center justify-between">
+        <div className="flex items-center gap-2">
+          <Logo fill="#1C1A22" />
+
+          <p
+            className="border border-white-neutral-light-300 rounded-2xs px-2 py-1 bg-white-neutral-light-200"
+            style={{
+              backgroundImage: `repeating-linear-gradient(
+                45deg,
+                rgba(0,0,0,0.1) 0px,
+                rgba(0,0,0,0.1) 1px,
+                transparent 1px,
+                transparent 3px
+              )`,
+            }}
+          >
+            Gerador de Proposta
+          </p>
+        </div>
+
+        <Link
+          href="/dashboard"
+          className="h-[44px] w-[44px] border border-white-neutral-light-300 hover:bg-white-neutral-light-200 bg-white-neutral-light-100 rounded-[10px] flex items-center justify-center button-inner cursor-pointer"
+        >
+          <CloseIcon width="10" height="10" fill="#1C1A22" />
+        </Link>
+      </nav>
+
+      <main className="flex-1 p-7">{children}</main>
+    </div>
+  );
+}
