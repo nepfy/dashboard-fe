a1b6f4e8207ca43ed4c85c812f457166eb675b95
Caio Alcantara <alcantaracaiolucas@gmail.com>
2025-10-01 19:57:31 -0700

    style: page layout

--- src/app/admin/templates/config/[templateId]/page.tsx
+++ src/app/admin/templates/config/[templateId]/page.tsx
@@ -99,7 +99,7 @@ export default function TemplateConfigDetailPage() {
 
   return (
     <div className="min-h-screen bg-gray-50 p-8">
-      <div className="max-w-7xl mx-auto">
+      <div className="mx-auto">
         <div className="mb-8 flex items-center justify-between">
           <div>
             <button
