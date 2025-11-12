588751490f003a5e228b346e0efa664b16039645
Caio Alcantara <alcantaracaiolucas@gmail.com>
2025-11-12 10:09:19 -0800

    fix: typo

--- src/app/project/[userName]/[projectUrl]/page.tsx
+++ src/app/project/[userName]/[projectUrl]/page.tsx
@@ -3,14 +3,14 @@ import FlashTemplate from "#/app/project/components/Templates/Flash";
 import { getProjectData } from "#/app/project/services/project-data";
 
 type ProjectPageProps = {
-  params: {
+  params: Promise<{
     userName: string;
     projectUrl: string;
-  };
+  }>;
 };
 
 export default async function ProjectPage({ params }: ProjectPageProps) {
-  const { userName, projectUrl } = params;
+  const { userName, projectUrl } = await params;
 
   const projectData = await getProjectData(userName, projectUrl);
 
